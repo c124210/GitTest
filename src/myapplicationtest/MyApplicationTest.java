c5cd88a2689b9078460f733692da23294c42af4a
Paul Willies <paulj.willies@gmail.com>
2022-04-12 13:42:22 +0200

    my git test

--- src/myapplicationtest/MyApplicationTest.java
+++ src/myapplicationtest/MyApplicationTest.java
@@ -17,6 +17,8 @@ public class MyApplicationTest {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Testing... ");
+        
+        // we are testing functionality
     }
     
 }
